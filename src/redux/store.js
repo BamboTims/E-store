c8afdc34c7cebd1c86cc420bb7c0c4ee8b616587
BamboTims <timilehinadebambo@gmail.com>
2022-09-06 14:41:07 +0100

    root-saga update

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,9 @@ import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import persistStore from "redux-persist/es/persistStore";
 import createSagaMiddleware from "redux-saga";
+import rootSaga from "./root-saga";
 
 import rootReducer from "./root.reducer";
-import { fetchCollectionStart } from "./shop/shop.saga";
-
 const sagaMiddleware = createSagaMiddleware();
 
 const middleware = [sagaMiddleware];
@@ -14,7 +13,6 @@ if (process.env.NODE_ENV === "development") {
 	middleware.push(logger);
 }
 
-
 export const store = createStore(rootReducer, applyMiddleware(...middleware));
 export const persistor = persistStore(store);
-sagaMiddleware.run(fetchCollectionStart);
+sagaMiddleware.run(rootSaga);
