d831601a69d2f3916c6af9ef4187d14ace8d675f
BamboTims <timilehinadebambo@gmail.com>
2022-09-29 12:42:33 +0100

    netlify fix

---
+++ client/build/precache-manifest.f541b224d909d2c530b6f8663e745307.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "f98a72a48a7875506744",
+    "url": "/static/css/main.db82e96d.chunk.css"
+  },
+  {
+    "revision": "f98a72a48a7875506744",
+    "url": "/static/js/main.f98a72a4.chunk.js"
+  },
+  {
+    "revision": "829f2d0e0f127944c09b",
+    "url": "/static/js/1.829f2d0e.chunk.js"
+  },
+  {
+    "revision": "ca3a14fdfe4b38fab5fd",
+    "url": "/static/css/2.180d976f.chunk.css"
+  },
+  {
+    "revision": "ca3a14fdfe4b38fab5fd",
+    "url": "/static/js/2.ca3a14fd.chunk.js"
+  },
+  {
+    "revision": "daf672644a2f88bc1e24",
+    "url": "/static/css/3.d987fc19.chunk.css"
+  },
+  {
+    "revision": "daf672644a2f88bc1e24",
+    "url": "/static/js/3.daf67264.chunk.js"
+  },
+  {
+    "revision": "f3b1c56d86e2980a46d6",
+    "url": "/static/css/4.2a6ad892.chunk.css"
+  },
+  {
+    "revision": "f3b1c56d86e2980a46d6",
+    "url": "/static/js/4.f3b1c56d.chunk.js"
+  },
+  {
+    "revision": "bda9d3878e4199c03855",
+    "url": "/static/css/5.46abd0c0.chunk.css"
+  },
+  {
+    "revision": "bda9d3878e4199c03855",
+    "url": "/static/js/5.bda9d387.chunk.js"
+  },
+  {
+    "revision": "79defc73eb7be3d5340d",
+    "url": "/static/css/6.3f0645ef.chunk.css"
+  },
+  {
+    "revision": "79defc73eb7be3d5340d",
+    "url": "/static/js/6.79defc73.chunk.js"
+  },
+  {
+    "revision": "e2c7e09264ccdb7fb4b8",
+    "url": "/static/js/7.e2c7e092.chunk.js"
+  },
+  {
+    "revision": "424f137a52450379af51",
+    "url": "/static/js/8.424f137a.chunk.js"
+  },
+  {
+    "revision": "fed296737abc0b94dfab",
+    "url": "/static/js/runtime~main.fed29673.js"
+  },
+  {
+    "revision": "fe32adeddb912787018825230c35d0e9",
+    "url": "/static/media/Larkef.fe32aded.svg"
+  },
+  {
+    "revision": "9411305177a66354cbc3b20300c265b3",
+    "url": "/static/media/shopping-bag.94113051.svg"
+  },
+  {
+    "revision": "c2601387e13fbd4deeca74e1bcee2ca8",
+    "url": "/static/media/shopping-cart.c2601387.svg"
+  },
+  {
+    "revision": "545763ac179f8a157ca600488f405438",
+    "url": "/static/media/trash-svgrepo-com.545763ac.svg"
+  },
+  {
+    "revision": "5cf3790e2078d5bc995e5a24fe7a7dde",
+    "url": "/static/media/google-svgrepo-com.5cf3790e.svg"
+  },
+  {
+    "revision": "c3de75b47ff1e0331af2e50956e6869e",
+    "url": "/index.html"
+  }
+];
